46e545934836b4e1b726e4690582a3809be5c9d5
Gregory Noack <greg@squishymedia.com>
2016-05-18 13:44:42 -0700

    making changes to happybrowser path

--- thinknoack/theme/happybrowser/happybrowser-nojquery.js
+++ thinknoack/theme/happybrowser/happybrowser-nojquery.js
@@ -6,24 +6,24 @@ onload = function(){
     if (bowser.msie && bowser.version <= 9) {
       hbDiv.innerHTML = '<div class="hb-wrap clearfix">'+
                         '<div class="hb-text clearfix">'+
-                          '<img src="/happybrowser/img/warning.jpg" alt="warning" /> '+
+                          '<img src="/theme/happybrowser/img/warning.jpg" alt="warning" /> '+
                           '<p>We\'ve detected that you have an outdated browser. This site may not work as expected. <strong>Please consider upgrading your browser.</strong></p>'+
                         '</div>'+
                         '<div class="hb-upgrade clearfix">'+
                           '<div class="hb-img-wrap hb-dismiss" id="dismiss">X</div>'+
                           '<div class="hb-img-wrap hb-chrome">'+
                             '<a href="http://www.google.com/chrome/" >'+
-                              '<img src="/happybrowser/img/chrome.jpg" alt="upgrade chrome"/>'+
+                              '<img src="/theme/happybrowser/img/chrome.jpg" alt="upgrade chrome"/>'+
                               '</a>'+
                           '</div>'+
                           '<div class="hb-img-wrap hb-ff">'+
                             '<a href="https://www.mozilla.org/en-US/firefox/new/?utm_source=firefox-com&utm_medium=referral" >'+
-                            '<img src="/happybrowser/img/ff.jpg" alt="upgrade firefox"/>'+
+                            '<img src="/theme/happybrowser/img/ff.jpg" alt="upgrade firefox"/>'+
                             '</a>'+
                           '</div>'+
                           '<div class="hb-img-wrap hb-ie">'+
                             '<a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie" >'+
-                            '<img src="/happybrowser/img/ie.jpg" alt="upgrade internet explorer"/>'+
+                            '<img src="/theme/happybrowser/img/ie.jpg" alt="upgrade internet explorer"/>'+
                             '</a>'+
                           '</div>'+
                         '</div>'+
@@ -32,19 +32,19 @@ onload = function(){
     }else if(bowser.c && !bowser.msie){
       hbDiv.innerHTML = '<div class="hb-wrap clearfix">'+
                           '<div class="hb-text clearfix">'+
-                            '<img src="/happybrowser/img/warning.jpg" alt="warning" />'+
+                            '<img src="/theme/happybrowser/img/warning.jpg" alt="warning" />'+
                             '<p>We\'ve detected that you have an outdated browser. This site may not work as expected. <strong>Please consider upgrading your browser.</strong></p>'+
                           '</div>'+
                           '<div class="hb-upgrade clearfix">'+
                             '<div class="hb-img-wrap hb-dismiss" id="dismiss">X</div>'+
                             '<div class="hb-img-wrap hb-chrome">'+
                               '<a href="http://www.google.com/chrome/" >'+
-                                '<img src="/happybrowser/img/chrome.jpg" alt="upgrade chrome"/>'+
+                                '<img src="/theme/happybrowser/img/chrome.jpg" alt="upgrade chrome"/>'+
                               '</a>'+
                             '</div>'+
                            ' <div class="hb-img-wrap hb-ff">'+
                               '<a href="https://www.mozilla.org/en-US/firefox/new/?utm_source=firefox-com&utm_medium=referral" >'+
-                                '<img src="/happybrowser/img/ff.jpg" alt="upgrade firefox"/>'+
+                                '<img src="/theme/happybrowser/img/ff.jpg" alt="upgrade firefox"/>'+
                               '</a>'+
                             '</div>'+
                           '</div>'+
--- thinknoack/theme/happybrowser/happybrowser.js
+++ thinknoack/theme/happybrowser/happybrowser.js
@@ -4,13 +4,13 @@
     if (bowser.msie && bowser.version <= 8) {
         $('body').addClass('deadBrowser');
         $('<div class="hbMarkup"></div>').prependTo('body');
-        $('.hbMarkup').load( "/sites/district/themes/blp_boot/js/happybrowser/happybrowserIE.html" );
+        $('.hbMarkup').load( "/theme/happybrowser/happybrowserIE.html" );
 
       }
       else if (bowser.c && !bowser.msie) {
         $('body').addClass('deadBrowser');
         $('<div class="hbMarkup"></div>').prependTo('body');
-        $('.hbMarkup').load( "/sites/district/themes/blp_boot/js/happybrowser/happybrowser.html" );
+        $('.hbMarkup').load( "/theme/happybrowser/happybrowser.html" );
 
       }
     }
